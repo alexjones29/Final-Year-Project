6bab6b65c70d5eae8a9bc5c8975f5f6e8ec82830
unknown <aaj10@psvws423.pau.local>
2017-02-21 16:49:51 +0000

    Added functionality to read in cipher text

--- CipherCracker/src/main/CrackerApplication.java
+++ CipherCracker/src/main/CrackerApplication.java
@@ -1,19 +1,29 @@
 package main;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CrackerApplication
 {
 
-	ArrayList<CipherSymbol> cipherText = new ArrayList<CipherSymbol>();
+	List cipherText = new ArrayList<CipherSymbol>();
 
 	public CrackerApplication() 
 	{
 		
 	}
+	
+	public void readInCiphertext()
+	{
+		CiphertextReader cipherReader = new CiphertextReader();
+		File cipher = new File("resources/340cipherascii.txt");
+		cipherText = cipherReader.readInCipherText(cipher);
+		
+	}
 
 	public void readInDictionary()
 	{
-
+			
 	}
 }
--- CipherCracker/src/main/RunApplication.java
+++ CipherCracker/src/main/RunApplication.java
@@ -6,6 +6,7 @@ public class RunApplication
 	public static void main(String[] args)
 	{
 		CrackerApplication app = new CrackerApplication();
+		app.readInCiphertext();
 	}
 
 }
